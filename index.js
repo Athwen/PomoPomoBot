00ed2711717a448635e49adb6661ef7207f5921a
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-16 14:09:23 -0500

    Allows bot to come online

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () =>{
+    console.log('Ready!');
+
+});
+
+client.login('ODAwMDIyNjA1OTAxMjAxNDM4.YAMFHA.Ys7vwIJtJy9sSlsNOaHCduq7NKE');
