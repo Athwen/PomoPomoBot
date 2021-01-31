43b6689c88c767524f29b563f517a79410a5a3eb
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-31 14:24:57 -0500

    should ping a user when pinging

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,11 @@
+
 module.exports = {
     name: 'ping',
     description: 'Ping!',
     args: true,
     usage: '<user>',
     execute(message, args){
-        
+        const user = message.client.users.cache.find(user => user.name === `${args[0]}`);
         message.channel.send(`ping @${args[0]}`);
 
     },
