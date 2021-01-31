b8ddf1e86f1a6a3b2a76a1c51aa7866936a2d230
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-31 14:53:51 -0500

    maybe fixes mention

--- commands/ping.js
+++ commands/ping.js
@@ -5,10 +5,7 @@ module.exports = {
     args: true,
     usage: '<user>',
     execute(message, args){
-        message.guild.members.fetch();
-        const user = message.client.users.cache.find(user => user.name === `${args[0]}`);
-        console.log(user);
-        message.channel.send(`ping ${user}`);
+        message.channel.send(`ping @${args[0]}`);
 
     },
 };
