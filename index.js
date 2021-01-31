6988b603fab4fed60c7f44b5152cd82948e2203a
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-31 13:05:24 -0500

    following guide from discordjs for args

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { prefix } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () =>{
@@ -7,11 +8,24 @@ client.once('ready', () =>{
 });
 
 client.on('message', message => {
-    if(message.content === 'ping'){
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split();
+    const command = args.shift().toLower();
+
+    if(command === 'ping'){
         message.reply('pong');
+
+    }else if(command = 'args-info'){
+        if(!args.length){
+            return message.channel.send(`You didn't provide any arguments, ${message.author}`);
+
+        }
+
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
 
     }
 
-});
+})
 
 client.login(process.env.BOT_TOKEN);
