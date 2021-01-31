9ae17bed3d324adf0dba429b6775e15cc98ffd87
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-31 14:37:21 -0500

    adds guild.fetch()

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,7 @@ module.exports = {
     args: true,
     usage: '<user>',
     execute(message, args){
+        message.guild.fetch();
         const user = message.client.users.cache.find(user => user.name === `${args[0]}`).username;
         message.channel.send(`ping ${user}`);
 
