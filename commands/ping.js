6683aa304a0b30b01ee98041917ef281ec07c131
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-31 13:37:02 -0500

    attempting to add expected command usage

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,20 @@
 module.exports = {
     name: 'ping',
     description: 'Ping!',
+    args: true,
+    usage: '<user>',
     execute(message, args){
+        if(command.args && !args.length){
+            let reply = `You didn't provide any arguments, ${message.author}`;
+
+            if(command.usage){
+                reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+
+            }
+
+            return message.channel.send(reply);
+
+        }
         message.channel.send('Pong.');
 
     },
--- index.js
+++ index.js
@@ -24,12 +24,14 @@ client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).trim().split(' ');
-    const command = args.shift().toLowerCase();
+    const commandName = args.shift().toLowerCase();
 
-    if(!client.commands.has(command)) return;
+    if(!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(); 
 
     try{
-        client.commands.get(command).execute(message, args);
+        command.execute(message, args);
 
     }catch (error){
         console.log(error);
