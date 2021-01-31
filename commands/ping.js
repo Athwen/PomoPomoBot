ebf5f41b6b67cb26c592539a0491a9e0c2610f1f
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-01-31 14:49:18 -0500

    attempts to fix

--- commands/ping.js
+++ commands/ping.js
@@ -5,8 +5,9 @@ module.exports = {
     args: true,
     usage: '<user>',
     execute(message, args){
-        message.client.users.fetch();
-        const user = message.client.users.cache.find(user => user.name === `${args[0]}`).username;
+        message.guild.members.fetch();
+        const user = message.client.users.cache.find(user => user.name === `${args[0]}`);
+        console.log(user);
         message.channel.send(`ping ${user}`);
 
     },
