154abdeb2483f01a695d17a6d7ad23af6037d28f
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-02-05 18:14:30 -0500

    adds music command

--- commands/ping.js
+++ commands/ping.js
@@ -5,11 +5,9 @@ module.exports = {
     args: true,
     usage: '<user>',
     execute(message, args){
-        message.guild.members.fetch().then()
         console.log(message.client.users.cache);
         const user = message.client.users.cache.find(user => user.username === `${args[0]}`);
-        console.log(user);
         message.channel.send(`ping ${user}`);
 
     },
-};
+};  
---
+++ commands/play.js
@@ -0,0 +1,25 @@
+const { VoiceConnection, MessageManager } = require('discord.js');
+const ytdl  = require('ytdl-core');
+
+module.exports = {
+    name: 'play',
+    description: 'play music from youtube link',
+    args: true,
+    usage: '<Youtube URL>',
+    execute(message, args){
+        if(message.member.author.voice.channel){
+            const connection = await message.member.author.channel.join();
+
+            connection.play(ytdl(args[0], { filter: 'audioonly'}), {
+                type: 'webm/opus'
+
+            });
+
+        }else{
+            message.reply('You need to be inside of a voice channel to play music');
+
+        }
+
+    },
+
+};
