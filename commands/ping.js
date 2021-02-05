624d3e9ebd9f0a7e73a81de86bd98dfc6254ef90
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-02-05 13:43:30 -0500

    fixes target user

--- commands/ping.js
+++ commands/ping.js
@@ -8,9 +8,9 @@ module.exports = {
         message.guild.members.fetch().then(fetchedMembers =>{
             const onlineUsers = fetchedMembers.filter(member => member.presence.status !== 'offline');
 
-            console.log(onlineUsers);
+            console.log(fetchedMembers);
 
-            const user = onlineUsers.find(user => user.username === `${args[0]}`);
+            const user = onlineUsers.find(target => target.user.username === `${args[0]}`);
             console.log(user);
             message.channel.send(`ping ${user}`);
 
