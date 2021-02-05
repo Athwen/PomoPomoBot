1eddf3fff7133ed92038398dee45213ff1b49724
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-02-05 11:43:27 -0500

    printing our caches users

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,10 @@ module.exports = {
     args: true,
     usage: '<user>',
     execute(message, args){
+        message.guild.members.fetch();
+        console.log(message.client.users);
+        const users = message.client.users.cache.find(user => user.name === `${args[0]}`);
+        console.log(users);
         message.channel.send(`ping @${args[0]}`);
 
     },
